ebbb97fc554c9f4facecd701b21b6c6ee59fc37d
sullivancott <sullivancott@outlook.com>
2021-08-17 14:45:49 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Client, Collection } = require('discord.js');
 const bot = new Client();
 const { prefix } = require(`./config.json`);
+[`aliases`, `commands`].forEach(x => bot[x] = new Collection());
+["command", "events"].forEach(x => require(`./handlers/${x}`)(bot));
 bot.login(process.env.token);
 
 bot.on("ready", () => {
